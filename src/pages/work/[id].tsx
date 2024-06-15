d974f71402d884bb4b181e32edad63693c5e86ee
r7projects-shayan <shayansworks@gmail.com>
2024-06-15 16:59:23 +0530

    newfinal22

--- src/pages/work/[id].tsx
+++ src/pages/work/[id].tsx
@@ -1,1 +1,19 @@
-export {};
+
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const WorkDetailsPage = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    
+
+    return (
+        <div>
+            
+           
+        </div>
+    );
+};
+
+export default WorkDetailsPage;
